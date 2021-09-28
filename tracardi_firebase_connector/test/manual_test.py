29d507ea786f14036eb92d04d936c38747898969
emge1 <88485481+emge1@users.noreply.github.com>
2021-09-28 17:55:44 +0200

    Update manual_test.py

--- tracardi_firebase_connector/test/manual_test.py
+++ tracardi_firebase_connector/test/manual_test.py
@@ -1,24 +1,26 @@
+from tracardi.domain.context import Context
+from tracardi.domain.entity import Entity
+from tracardi.domain.event import Event
+from tracardi.domain.profile import Profile
+from tracardi.domain.session import Session
 from tracardi_plugin_sdk.service.plugin_runner import run_plugin
 
 from tracardi_firebase_connector.plugin import FirebaseConnectorAction
 
-init = {
-    "authentication": {
-        "email": None,
-        "password": None
-    },
-    "connection": {
-        "apiKey": None,
-        "authDomain": None,
-        "databaseURL": None,
-        "storageBucket": None,
-        "serviceAccount": None,
-        "query": None
+init={
+            "email": "emygeq@gmail.com",
+            "password": "1111",  #przy podaniu złego hasła jest błąd 400 - INVALID_PASSWORD
+            "apiKey": "AIzaSyBRjfXq5Au22x63hoW95-4mnvFdKPileRQ",
+            "authDomain": "test-9267b.firebaseapp.com",
+            "databaseURL": "https://console.firebase.google.com/project/test-9267b",
+            "storageBucket": "test-9267b.appspot.com",
+            "serviceAccount": None,
+            "query": {
+                'some': 'data'
+            }
     }
-}
 
-payload = {
-
-}
+payload = {}
 
 result = run_plugin(FirebaseConnectorAction, init, payload)
+print(result)
