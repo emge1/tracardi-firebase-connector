d2213679af8c75e7dd615b34741df3e27930faa7
emge1 <88485481+emge1@users.noreply.github.com>
2021-09-28 17:56:32 +0200

    Update configuration.py

--- tracardi_firebase_connector/model/configuration.py
+++ tracardi_firebase_connector/model/configuration.py
@@ -1,32 +1,15 @@
 from pydantic import BaseModel
 from typing import Optional
-from firebase import Firebase
 
 
-class FirebaseAuthentication(BaseModel):
+class FirebaseConfiguration(BaseModel):
     email: str
     password: str
 
-    async def authentication(self):
-        auth = firebase.auth()
-        return await auth.sign_in_with_email_and_password(email=self.email,
-                                                          password=self.password)
-
-
-class FirebaseConnection(BaseModel):
     apiKey: str
     authDomain: str
     databaseURL: str
     storageBucket: str
     serviceAccount: Optional[str]
 
-    async def connect(self):
-        return await Firebase.config(apiKey=self.apiKey,
-                                     authDomain=self.authDomain,
-                                     databaseURL=self.databaseURL,
-                                     storageBucket=self.storageBucket,
-                                     serviceAccount=self.serviceAccount)
-
-
-class PluginConfiguration(BaseModel):
-    query: str
+    query: dict
